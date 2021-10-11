ca7b020feb0348dc1e542042ecc6d421d4768532
Tatiana Karasyova <tatiana.karasyova@kohls.com>
2021-10-11 13:06:21 +0200

    tutorials added

--- search_with_filtering.py
+++ search_with_filtering.py
@@ -1,14 +1,13 @@
 import random
 import string
+import setup_catalog
 
 from google.api_core.client_options import ClientOptions
-from google.cloud.retail_v2 import SearchServiceClient, SearchRequest
 
-import setup_catalog
+from google.cloud.retail_v2 import SearchServiceClient, SearchRequest
 
 project_number = "SET HERE VALID PROJECT NUMBER"
 endpoint = "retail.googleapis.com"
-isolation_filter_key = "INTEGRATION_FILTER_KEY"
 title_query = "Nest_Maxi"
 visitor_id = "visitor"
 test_id = ''.join(random.sample(string.ascii_lowercase, 1))
@@ -25,35 +24,30 @@ def get_search_service_client():
 # [END search_client]
 
 
-def build_isolation_filter(test__id: str):
-    return 'attributes.{0}: ANY("{1}")'.format(isolation_filter_key, test__id)
-
-
-# [START search_product_with_order]
-def search_ordered_products(query: str, order: str, page_size=10):
+# [START search_product_with_filter]
+def search_filtered_products(query: str, _filter: str, page_size=10):
     search_request = SearchRequest()
     search_request.placement = default_search_placement
     search_request.query = query
-    search_request.filter = build_isolation_filter(test_id)
-    search_request.order_by = order
+    search_request.filter = _filter
     search_request.page_size = page_size
     search_request.visitor_id = visitor_id
-    print("---search request---")
+    print("---search_request:---")
     print(search_request)
 
     return get_search_service_client().search(search_request)
 
 
-# [END search_product_with_order]
+# [END search_product_with_filter]
 
 def search():
     setup_catalog.ingest_products(test_id)
-
-    "# [TRY DIFFERENT ORDERING EXPRESSIONS HERE:]" 
-    order = 'price desc'
     
-    search_response = search_ordered_products(title_query, order)
-    print("---ordered search results---")
+    # [TRY DIFFERENT FILTER EXPRESSIONS HERE:]
+    filter = '(colorFamily: ANY("black"))'
+    
+    search_response = search_filtered_products(title_query, filter)
+    print("---filtered search response---")
     print(search_response)
 
     setup_catalog.delete_products()
